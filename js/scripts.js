85dabab0e103204d6f59708d5376ed0a0f48bc22
Gabriel Dias Felipe <129851145+GabrielDiasFelipe@users.noreply.github.com>
2023-07-16 21:33:36 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function buttomgit(){
+    document.getElementById('gitCard').style.display = "block"
+}
+function linkbuttom(){
+    document.getElementById('linkCard').style.display = "block"
+}
+function twitterbuttom(){
+    document.getElementById('twitterCard').style.display = "block"
+}
+function homebuttom(){
+    document.getElementById('socialCard').style.animationPlayState = "running" 
+}
